9d6f9b2bbdfce3af17ddc15d36007519ba4208ba
saladziakcloud <pawel.saladziak@icloud.com>
2026-08-27 01:20:17 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,14 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
-import remarkGfm from 'remark-gfm'
-import remarkSmartypants from 'remark-smartypants'
-import rehypeExternalLinks from 'rehype-external-links'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
-  markdown: {
-    shikiConfig: {
-      theme: 'nord',
-    },
-    remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
-})
+  site: 'https://itakura.studio',
+  output: 'server',
+  adapter: cloudflare({
+    mode: 'directory'
+  }),
+  integrations: [svelte(), mdx()]
+});
